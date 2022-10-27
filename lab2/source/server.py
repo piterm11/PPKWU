f85adab26a43d5240e96cf0635afab612c219e01
Piotr Marcinkowski <228800@edu.p.lodz.pl>
2022-10-27 13:10:20 +0200

    komenda rev

--- lab2/source/server.py
+++ lab2/source/server.py
@@ -21,16 +21,24 @@ class web_server(http.server.SimpleHTTPRequestHandler):
             text = b"Hello World!\n"
             self.wfile.write(text)
         elif self.path.startswith('/?cmd='):
-            path = self.path[6:]
+            param = self.path[6:]
             self.protocol_version = 'HTTP/1.1'
             self.send_response(200)
             self.send_header("Content-type", "text/html; charset=UTF-8")
             self.end_headers()    
-            if path=='time':
+            if param=='time':
                 warsawTimeZone = pytz.timezone("Europe/Warsaw") 
                 warsawTime = datetime.now(warsawTimeZone)
                 text = (warsawTime.strftime('%H:%M:%S')+"\n").encode()
                 self.wfile.write(text)
+            elif param.startswith('rev'):
+                param2 = param.removeprefix('rev')
+                if param2.startswith('&str='):
+                    param2 =param2.removeprefix('&str=')
+                    text = (param2[::-1]+'\n').encode()
+                    self.wfile.write(text)
+                else:
+                    self.wfile.write(b"wrong command\n")
             else:
                 self.wfile.write(b"wrong command\n")
         else:
