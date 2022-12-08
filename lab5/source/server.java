f98e132864ef2001f07e02793876b11195ba43c3
Piotr Marcinkowski <228800@edu.p.lodz.pl>
2022-12-08 12:26:17 +0100

    init lab 5

---
+++ lab5/source/server.java
@@ -0,0 +1,50 @@
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+public class Test {
+
+	public static void main(String[] args) throws Exception {
+		HttpServer server = HttpServer.create(new InetSocketAddress(4080), 0);
+		server.createContext("/", new MyHandler());
+		server.setExecutor(null); // creates a default executor
+		server.start();
+	}
+
+	static class MyHandler implements HttpHandler {
+		@Override
+		public void handle(HttpExchange t) throws IOException {
+			Map<String,String> params = queryToMap(t.getRequestURI().getQuery());
+			String response = "Hello World";
+			
+			t.sendResponseHeaders(200, response.length());
+			OutputStream os = t.getResponseBody();
+			os.write(response.getBytes());
+			os.close();
+		}
+
+		public Map<String, String> queryToMap(String query) {
+			if(query == null) {
+				return null;
+			}
+			Map<String, String> result = new HashMap<>();
+			for (String param : query.split("&")) {
+				String[] entry = param.split("=");
+				if (entry.length > 1) {
+					result.put(entry[0], entry[1]);
+				}else{
+					result.put(entry[0], "");
+				}
+			}
+			return result;
+		}
+
+
+	}
+}
